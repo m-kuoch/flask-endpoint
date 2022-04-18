947fd87cbd7f84a146badfe08cef398506359bfb
m-kuoch <m.kuoch1@gmail.com>
2022-04-17 20:00:15 -0400

    app.py setup

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
+"""App with candles endpoint."""
+from datetime import timedelta
 from flask import Flask
+from flask import request
+import json
+
+from coinbase_api import get_one_minute_candles_from_coinbase
+from utils import check_if_valid_iso, get_valid_currency_pairs
+
+INVALID_TIME_MSG = 'Invalid time format. ' \
+                   'Please use ISO 8601 and specify up to the minute.'
 
 app = Flask(__name__)
+valid_pairs = []
+load_pairs_success = False
 
 
 @app.route('/')
@@ -12,8 +24,15 @@ def testing():
 @app.route('/testing2')
 def testing2():
     """Testing another route."""
+    print(request.args)
     return 'Hello again!'
 
 
 if __name__ == '__main__':
+    try:
+        valid_pairs = get_valid_currency_pairs()
+        load_pairs_success = True
+    except:
+        print("Error loading currency pairs")
+
     app.run(host='0.0.0.0', port=5001)
