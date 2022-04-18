52252bd73f1592aeee443bc4c1a674ffc0161a1a
m-kuoch <m.kuoch1@gmail.com>
2022-04-17 21:22:06 -0400

    added long interval tests

--- tests/test_time_parameters.py
+++ tests/test_time_parameters.py
@@ -32,6 +32,15 @@ def test_long_interval(client):
     response = client.get(
         "/candles?currency_pair=ETH-USD"
         "&start_time=2019-11-02T00:00:00"
+        "&end_time=2019-11-03T00:00:00"
+    )
+    assert 200 == response.status_code
+
+
+def test_too_long_interval(client):
+    response = client.get(
+        "/candles?currency_pair=ETH-USD"
+        "&start_time=2019-11-02T00:00:00"
         "&end_time=2019-11-03T00:01:00"
     )
     assert b"Time interval is too long (greater than 24 hours)." == response.data
